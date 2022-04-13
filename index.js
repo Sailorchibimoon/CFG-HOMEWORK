e33d51918d2eef7a7fba54f5cb647eed08237fc2
Sailorchibimoon <100092821+Sailorchibimoon@users.noreply.github.com>
2022-04-13 18:15:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+ //$("#topbutton").on("click", function() {
+  //  $("body").scrollTop(0);
+//});
+
+
+
+// document.querySelectorAll(".carousel").forEach(carousel => { //this selects all carousels on all pages
+   // const items = carousel.querySelectorAll(".carouselitem");
+   // const buttonsHtml = Array.from(items, ()=> { // this creates an array from the carousel button span class in the html page
+   // return `<span class="carouselbutton"></span>`
+   // });
+    
+    //carousel.insertAdjacentHTML("beforeend", //inserts buttons from array (items=4), before end is before end of html element
+//    `
+//    <div class="carouselnav"> 
+//    ${ buttonsHtml.join ("")}  
+//    </div>
+ //   `
+//    ); // will join all html strings e.g. span class, gives html for whol navigation section
+//    console.log(buttonsHtml); //saving buttonsHtml array
+  //  })
+
+ //   const buttons = carousel.querySelectorAll(".carousel__button");
+
+   // buttons.forEach((button, i) => {
+//      button.addEventListener("click", () => {
+        // un-select all the items
+ //       items.forEach((item) =>
+ //         item.classList.remove("carousel__item--selected")
+ //       );
+ //       buttons.forEach((button) =>
+ //         button.classList.remove("carousel__button--selected")
+//        );
+ // 
+//        items[i].classList.add("carousel__item--selected");
+//        button.classList.add("carousel__button--selected");
+//      });
+   
+  
+    // Select the first item on page load
+ //   items[0].classList.add("carousel__item--selected");
+//    buttons[0].classList.add("carousel__button--selected");
+// });
+  
